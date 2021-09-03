b60b26a14fbfc3678cd9784d5e590705a8a1e135
mahesh <mannemmahesh1996@gmail.com>
2021-09-03 13:18:10 +0530

    added file

---
+++ src/main/java/com/htc/ContactManager/Main.java
@@ -0,0 +1,15 @@
+package com.htc.ContactManager;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				ContactManager contactManager= new ContactManager();
+				contactManager.addContact("Mahesh	", "chirala", "9959009195");
+				System.out.println(contactManager.getAllContacts());
+				
+			}
+
+	}
+
+
